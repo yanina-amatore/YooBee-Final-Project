528361e8db7b635c9066262abc49a4cb3e7d5302
Yanina <99714877+Yaninaamatore@users.noreply.github.com>
2022-04-19 22:02:10 +1200

    Final assesment completed - final commit

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-document.querySelector('[data-value-btn]').onclick = () => {
-	const value = document.querySelector('[data-value-input]').value ;
-	const ul = document.querySelector('[data-todos]');
 
 
-	const li = document.createElement('li');
-	li.textContent = value
-	ul.appendChild(li)
+
+
+
  
- 
-
-}
-
